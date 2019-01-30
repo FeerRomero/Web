78bb4a3e3019ed532ff48b316e45f9ddcdfef30c
Fernando David Romero Nava <ferdav19@gmail.com>
2019-01-29 20:03:54 -0600

    Funcionalidad terminada

--- Lab2/javascript/javascript.js
+++ Lab2/javascript/javascript.js
@@ -1,7 +1,6 @@
 var itemNum = 6;
 var inputItem = document.getElementById("newitem");
-var todoList = document.querySelector('ul');
-
+var todoList = document.getElementById("todo_list");
 
 inputItem.addEventListener("keyup", function(evento) {
 	evento.preventDefault();
@@ -13,29 +12,50 @@ inputItem.addEventListener("keyup", function(evento) {
 		let span = document.createElement("SPAN");
 		let nuevoElemento = document.createElement("li");
 		let cbItem = document.createElement("INPUT");
-		
-		//Se añade al span el texto ingresado
-		span.appendChild(texto);
 
-		//Se asignan los valores al cbItem
-		cbItem.setAttribute("type", "checkbox");
-		cbItem.setAttribute("name", "todo");
-		cbItem.setAttribute("value", itemNum);
+		if(valueItem == "") {
+			window.alert("Nombre de actividad invalido.")
+		}
+		else
+		{
+			//Se añade al span el texto ingresado
+			span.appendChild(texto);
 
-		//Se añaden los elementos de cbItem y span al elemento de tipo "li"
-		nuevoElemento.appendChild(cbItem);
-		nuevoElemento.appendChild(span);
+			//Se asignan los valores al cbItem
+			cbItem.setAttribute("type", "checkbox");
+			cbItem.setAttribute("name", "todo");
+			cbItem.setAttribute("value", itemNum);
+			cbItem.setAttribute("onchange", "crossOut(this)");
 
-		//Se añade el nuevoElemento a la lista general
-		document.getElementById("todo_list").appendChild(nuevoElemento);
-		itemNum++;
+			//Se añaden los elementos de cbItem y span al elemento de tipo "li"
+			nuevoElemento.appendChild(cbItem);
+			nuevoElemento.appendChild(span);
+
+			//Se añade el nuevoElemento a la lista general
+			document.getElementById("todo_list").appendChild(nuevoElemento);
+			itemNum++;
+
+			//Limpiar el campo de texto
+			inputItem.value="";
+		}
 	}
 });
 
 
-//FALTA HACER LO DE CAMBIAR LA CLASE DEL SPAN CUANDO LE DAN CLICK AL CHECKBOX CORRESPONDIENTE.
-todoList.addEventListener('click', function(evento) {
-	if (evento.target.tagName === 'LI') {
-		evento.target.classList.toggle('done');
+function crossOut(checkbox) {
+	//Declaración de variables
+	let myTodoItems = todoList.getElementsByTagName('li');
+	let iPosition = checkbox.value;
+	let spanElement = myTodoItems[iPosition-1].childNodes[1];
+	
+	//Revisar si fue check o uncheck
+	if (checkbox.checked) {
+		//Añadir al span la clase "done"
+		spanElement.classList.add("done");
 	}
-}, false);
+	else
+	{
+		//Eliminar del span la clase "done"
+		spanElement.classList.remove("done");
+	}
+};
